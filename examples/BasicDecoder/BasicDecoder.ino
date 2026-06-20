ee6552068bb31504ef08bdc0e7941e850c89377c
Anonymous <>
2026-06-20 22:36:47 +0300

    Fix broken test vector in example

--- examples/BasicDecoder/BasicDecoder.ino
+++ examples/BasicDecoder/BasicDecoder.ino
@@ -1,18 +1,20 @@
 
 #include <RuuviTagDecoder.h>
 
+// Official RuuviTag Data Format 5 (RAWv2) test vector.
+// https://docs.ruuvi.com/communication/bluetooth-advertisements/data-format-5-rawv2#test-vectors
 uint8_t sampleData[] = {
     0x05,        // Format (5 = RAWv2)
     0x12, 0xFC,  // Temperature (+24.3°C)
-    0xC3, 0x74,  // Humidity (53.49%)
-    0x00, 0xD7,  // Pressure (1000.15 hPa)
-    0x03, 0xE8,  // Acceleration X (+1.000 G)
-    0x00, 0x00,  // Acceleration Y (0.000 G)
-    0x04, 0x00,  // Acceleration Z (+1.024 G)
-    0x0B, 0xAC,  // Battery (2.977V), TX Power (+4dBm)
-    0x64,        // Movement counter (100)
-    0x00, 0x04,  // Measurement sequence (4)
-    0xC3, 0x6E, 0xF8, 0x12, 0xAC, 0x5E  // MAC address
+    0x53, 0x94,  // Humidity (53.49%)
+    0xC3, 0x7C,  // Pressure (1000.44 hPa)
+    0x00, 0x04,  // Acceleration X (+0.004 G)
+    0xFF, 0xFC,  // Acceleration Y (-0.004 G)
+    0x04, 0x0C,  // Acceleration Z (+1.036 G)
+    0xAC, 0x36,  // Battery (2.977V), TX Power (+4dBm)
+    0x42,        // Movement counter (66)
+    0x00, 0xCD,  // Measurement sequence (205)
+    0xCB, 0xB8, 0x33, 0x4C, 0x88, 0x4F  // MAC address (CB:B8:33:4C:88:4F)
 };
 
 RuuviTagDecoder decoder;
@@ -45,7 +47,7 @@ void setup() {
         Serial.println(" hPa");
 
         Serial.print("Battery: ");
-        Serial.print(decoder.getBatteryVoltage());
+        Serial.print(decoder.getBatteryVoltage() / 1000.0f);  // getBatteryVoltage() returns mV
         Serial.println(" V");
 
     } else {
